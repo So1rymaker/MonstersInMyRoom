10815cf4cdd3a51ac65789a227744384d6ed7d1f
So1rymaker <278570069@qq.com>
2023-12-05 15:27:15 -0500

    Version2.0

---
+++ Assets/Script/HealthZeroOut.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class HealthZeroOut : MonoBehaviour
+{
+    public static int FailOrNot = 0;
+
+    public static void SetFailOrNot(int value)
+    {
+        FailOrNot = value;
+        if (FailOrNot == 1)
+        {
+            PlayerPrefs.SetInt("FAIL", FailOrNot);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("GameOverUI");
+        }
+    }
+}
